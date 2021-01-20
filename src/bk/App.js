d1c3ecfc8abea1b03b1116f6f5c28a37436f9828
integerzeroGit <favoritemail.integerzero@gmail.com>
2021-01-20 17:03:53 +0900

    display movie title

---
+++ src/bk/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class App extends React.Component {
+  state ={
+    count: 0
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  plus = () => {
+    this.setState(current => ({ count : current.count + 1 }))
+  }
+
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }))
+  }
+
+  render(){
+    return (<div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.plus}>plus</button>
+      <button onClick={this.minus}>minus</button>
+    </div>)
+  }
+}
+
+export default App;
